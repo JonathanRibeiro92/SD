568f0af8b0cb09c71f74e247356c12592156c402
Jay-Robot <jota.ribeiro.silva@gmail.com>
2018-04-20 18:18:46 -0300

    Colocação de Threads no código.

--- Chat/client.c
+++ Chat/client.c
@@ -5,14 +5,56 @@
 #include <netdb.h> 
 #include <string.h>
 
+pthread_t listener, writer;
+
 void error(char *msg)
 {
     perror(msg);
     exit(0);
 }
 
-int main(int argc, char *argv[])
-{
+
+void *receiveMessage(void *socket){
+    int sockfd, n, i;
+    char buffer[256];
+
+    sockfd = *(int *)socket;
+    bzero(buffer, 256);
+
+    while (1){
+        bzero(buffer,256);
+		n = read(sockfd,buffer,255);
+        if (n < 0) error("ERROR reading from socket");
+        printf("Message received: %s\n",buffer);
+
+    }
+    pthread_cancel(writer);
+    return NULL;
+}
+
+void *sendMessage (void *socket){
+    char buffer[256];
+    int sockfd, n;
+    
+    sockfd = *(int *)socket;
+    bzero(buffer,256);
+    
+    while (strcmp(buffer, "bye\n") != 0) {
+        bzero(buffer,256);
+        fgets(buffer,255,stdin);
+        n = write(sockfd,buffer, strlen(buffer)-1);
+        if (n < 0) error("ERROR reading from socket");
+    }
+    
+    pthread_cancel(listener);
+    return NULL;
+}
+
+
+
+
+int main(int argc, char *argv[]){
+    
     int sockfd, portno, n;
 
     struct sockaddr_in serv_addr;
@@ -40,23 +82,16 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    while (1){
-	    printf("You: ");
-	    bzero(buffer,256);
-	    fgets(buffer,255,stdin);
-	    n = write(sockfd,buffer,strlen(buffer));
-	    if (n < 0) 
-	         error("ERROR writing to socket");
-	    bzero(buffer,256);
-	    n = read(sockfd,buffer,255);
-	    if (n < 0) 
-	         error("ERROR reading from socket");
-		printf("Stranger: ");
-	    printf("%s\n",buffer);
-	    if (strcmp(buffer,"bye\n")==0) {
-			close(sockfd);
-			return 0;
-		}
-    }
+    
+    
+    pthread_create(&listener, NULL, receiveMessage, &sockfd);
+    pthread_create(&writer, NULL, sendMessage, &sockfd);
+    
+    pthread_join(listener, NULL);
+    pthread_join(writer, NULL);
+    
+    close(sockfd);
+   
+    return 0;
 
 }
